b852b0b42588210c83a42b3574053adf2c538981
Archana Yadav <85079777+archana01072003@users.noreply.github.com>
2023-08-03 11:26:10 -0700

    Update gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b)
+{
+    if(a == 0)
+    return b;
+    if(b == 0)
+    return a;
+    if(a == b)
+    return a;
 
+    if(a > b)
+    return gcd(a-b,b);
+    else
+    return gcd(a, b- a);
+
+}
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << "Hello Github"<< endl;
+    cout << "The GCD of " << a << " and "<<b << " is " << gcd(a,b)<<endl;
+    return 0;
+}
