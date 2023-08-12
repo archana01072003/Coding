9bee1e36a09f36fcf7ab6d6cc394a082108fa5a8
Archana Yadav <85079777+archana01072003@users.noreply.github.com>
2023-08-12 09:46:16 -0700

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+void bubbleSort(int* arr,int n)
+{
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Before sorting " << endl;
+    for(int i = 0; i<n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    bubbleSort(arr,n);
+    cout << "After sorting" << endl;
+    for(int i = 0; i<n;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
