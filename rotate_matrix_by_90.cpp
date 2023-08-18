d0e54f11d9afcce867afc464b8ec4824965653c6
Archana Yadav <85079777+archana01072003@users.noreply.github.com>
2023-08-18 10:14:03 -0700

    Create rotate_matrix_by_90.cpp

---
+++ rotate_matrix_by_90.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+void rotateMatrix(vector<vector<int >>&matrix) 
+{
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < i; j++) 
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for (int i = 0; i < n; i++) 
+    {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main() {
+    vector<vector<int>> arr;
+    arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    rotateMatrix(arr);
+    cout << "Rotated Matrix" << endl;
+    for (int i = 0; i < arr.size(); i++) 
+    {
+        for (int j = 0; j < arr[0].size(); j++) 
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
